b3fb31089f6d6f2edd7e1e74328ed93e8bc5547b
diegomdias87 <74568161+diegomdias87@users.noreply.github.com>
2023-08-31 10:43:55 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,18 @@
-/* eslint-disable no-promise-executor-return */
-/* eslint-disable import/prefer-default-export */
-const { VITE_API_KEY: API_KEY, VITE_API_URL: API_URL } = import.meta.env;
-
 export const getChatResponse = async (prompt: string): Promise<string> => {
-  // NEED ADD REQUEST API IMPLEMENTATION
+  const response = await fetch("https://45tswthhbi.execute-api.us-east-1.amazonaws.com/dev", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // Remove 'x-api-key' header
+      // 'x-api-key': API_KEY,
+    },
+    body: JSON.stringify({ prompt }),
+  });
 
-  // wait 2 second to simulate a real request to the API
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  if (!response.ok) {
+    throw new Error('Failed to fetch response from API.');
+  }
 
-  // throw new Error(
-  //   "Need to add API request implementation (look at src/api.ts)"
-  // );
-
-  return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+  const data = await response.json();
+  return data.response;
 };
